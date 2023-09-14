00beedac0d71b2e41e7d73d2a1e544d5f609669a
Juan Francisco Markan <jfmarkan@gmail.com>
2023-09-14 18:28:17 +0200

    Added specialization and booking seeder to DBSEEDER

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this -> call ([
+            SpecializationSeeder::class,
             SponsorshipSeeder::class,
             UserSeeder::class,
             HunterSeeder::class,
+            BookingMessageSeeder::class,
         ]);
     }
 }
