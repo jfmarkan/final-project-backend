eb78ba999f73678714e17bfbde3a70a4a4c89da5
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-09-14 16:53:21 +0200

    added hunters index and show

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\HunterController as ApiHunterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/hunters', [ApiHunterController::class, 'index'])->name('api.hunters.index');
+Route::get('/hunters/{hunter}', [ ApiHunterController::class, 'show' ])->name('api.hunters.show');
