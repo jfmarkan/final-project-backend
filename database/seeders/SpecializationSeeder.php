2440873be0d16588e2c707c85771b2f40d7610a1
GabrieleGrossi <grossigabriele98@gmail.com>
2023-09-14 18:25:20 +0200

    correction s

--- database/seeders/SpecializationSeeder.php
+++ database/seeders/SpecializationSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Model\Specialization;
+use App\Models\Specialization;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
