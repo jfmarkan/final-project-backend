258c1a72e74d428984c7e1c3bf00ab0a3a94c5fe
GabrieleGrossi <grossigabriele98@gmail.com>
2023-09-15 16:18:49 +0200

    Correction in HunterSeedr

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             HunterSeeder::class,
             BookingMessageSeeder::class,
+            SpecializationHunterSeeder::class,
         ]);
     }
 }
