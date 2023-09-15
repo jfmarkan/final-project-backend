5c6ce331d88d020080ee403a3b77d57db5231c2c
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-09-15 21:53:14 +0200

    added login and logout method to controllers

--- app/Http/Controllers/Api/GuestController.php
+++ app/Http/Controllers/Api/GuestController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Requests\LoginRequest;
 use App\Models\Hunter;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class GuestController extends Controller
 {
@@ -30,7 +32,7 @@ class GuestController extends Controller
         ]);
     }
 
-    public function store (Request $request){
+    public function signIn (Request $request){
         $data = $request->all();
 
         $user = User::create($data);
@@ -38,5 +40,32 @@ class GuestController extends Controller
         return response()->json([
             'success' => true
         ]);
+    }
+
+    public function login (LoginRequest $request){
+
+
+        $credentials = $request->validated();
+
+
+    // Tentativo di autenticazione dell'utente
+    if (Auth::attempt($credentials)) {
+        // Autenticazione riuscita
+        $user = Auth::user();
+        
+        // Genera un token di accesso
+        $token = $user->createToken('MyToken')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'access_token' => $token,
+        ]);
+    } 
+    else {
+        // Autenticazione fallita
+        return response()->json([
+            'error' => 'Credenziali non valide'
+        ],401);
+    }
     }
 }
