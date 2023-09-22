902f93c433c6c1daff494e8a6b9c49356d35a8a6
Juan Francisco Markan <jfmarkan@gmail.com>
2023-09-22 10:59:39 +0200

    fixed avarage vote error

--- app/Http/Controllers/Admin/HunterController.php
+++ app/Http/Controllers/Admin/HunterController.php
@@ -32,8 +32,9 @@ class HunterController extends Controller
 
         if($countReviews > 0){
             $averageVote = (intval($sumReviews)/$countReviews);
+        }else{
+            $averageVote = '-';
         }
-        $averageVote = '-';
         
         return view('admin.dashboard', compact('reviews','messages','countReviews','averageVote','totalMessages'));
     }
